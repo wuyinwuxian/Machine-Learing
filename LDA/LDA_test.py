a63d02496e0ddc7149eb163a7f0f34b42a036f71
liming <2944014083@qq.com>
2021-09-22 21:23:56 +0800

    更新LDA，线性判别分析实现

---
+++ LDA/LDA_test.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+'''
+函数说明:产生几个类别的随机样本
+Parameters:
+	无
+Returns:
+    Xi   -  第i类样本的组成的矩阵
+'''
+def createDataSet():
+    # 类别1
+    X1 = np.random.random((80, 4)) * 5 + 15
+    # 类别2
+    X2 = np.random.random((60, 4)) * 5 + 2
+    return X1, X2
+
+'''
+函数说明:计算一类样本的均值点
+Parameters:
+	dataset - 某个类别的样本矩阵
+Returns:
+    np.array(ave) - 这一类样本的中心（各个维度上的均值）
+'''
+def average(dataset):
+    ave = np.mean(dataset, axis=0)
+    return np.array(ave)
+
+'''
+函数说明:计算一类样本类内散度
+Parameters:
+	dataset - 某个类别的样本矩阵
+	ave     - 这一类样本的中心（各个维度上的均值）
+Returns:
+    sw      - 类内散度
+'''
+def compute_sw(dataset, ave):
+    n = dataset.shape[1]
+    sw = np.zeros((n, n))
+    for line in dataset:
+        x = np.array(line - ave).reshape(-1,1)
+        sw += np.dot(x,x.T)
+    return sw
+
+'''
+函数说明:计算所有类别样本的类间散度
+Parameters:
+	sample_number_arrary - 每个类别的样本个数向量
+	sample_mean_arrary   - 每个类别的均值向量构成的矩阵
+Returns:
+    sb      - 类间散度
+'''
+def compute_sb(sample_number_arrary, sample_mean_arrary):
+    n = sample_number_arrary.shape[1]
+    sb = np.zeros((n, n))
+    u = average(sample_mean_arrary)   # 全b局均值中心
+    for mi,ui in zip(sample_number_arrary,sample_mean_arrary):
+        x = np.array(ui - u).reshape(-1,1)
+        sb += np.dot(x,x.T)* mi
+    return sb
+
+
+
+if __name__ == '__main__':
+    """生成数据"""
+    X1, X2 = createDataSet()
+
+    """计算每一种类别的样本均值"""
+    u1 = average(X1)
+    u2 = average(X2)
+
+    """计算类内散度矩阵（每个都是特征*特征大小）"""
+    x1_sw = compute_sw(X1, u1)
+    x2_sw = compute_sw(X2, u2)
+    Sw = x1_sw + x2_sw
+
+    """求广义逆,再和均值之差相乘"""
+    pinv = np.linalg.pinv(Sw)
+    w = np.dot(pinv, u1 - u2)
+
+
+"""
+虽然西瓜书放在了线性模型这一张，但是我觉得他放在降维哪儿更适合
+输入：数据集 D = {(x1, y1),  (x2, y2), .... (xm, ym)}，其中任意样本 xi 为 n维向量， yi € {C1,  c2, ...Ck}，降维到的维度 d。
+输出：降维后的样本集 D'
+
+多类别：
+1）计算每一种类别的样本均值
+2）计算类内散度矩阵Sw
+3）计算类间散度矩阵 Sb   
+4）计算矩阵Sw-1Sb
+5) 计算 Sw-1Sb 的最大的 d个特征值和对应的 d个特征向量 （w1, w2, ... wd），得到投影矩阵 W
+6）对样本集中的每一个样本特征 xi，转化为新的样本 zi = WTxi
+7）得到输出样本集 D' = {(z1, y1),  (z2, y2), .... (zm, ym)}
+
+二类别：
+1）计算每一种类别的样本均值
+2）计算类内散度矩阵Sw
+3) 计算类Sw-1(u1-u2) = w 得到投影的线的权重（斜率） W
+4）对样本集中的每一个样本特征 xi，转化为新的样本 zi = WTxi
+5）得到输出样本集 D' = {(z1, y1),  (z2, y2), .... (zm, ym)}
+"""
